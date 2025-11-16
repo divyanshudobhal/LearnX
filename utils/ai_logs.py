7a8bfab8f3ada6ce834abbe68da5f0500b83e9be
Divyanshu Dobhal <dobhaldivyanshu5@gmail.com>
2025-11-16 15:26:41 +0530

    Update ai_logs.py

--- utils/ai_logs.py
+++ utils/ai_logs.py
@@ -1,7 +1,8 @@
 import json
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 AI_LOGS_FILE = os.path.join(BASE_DIR, "ai_logs.json")
 
 
